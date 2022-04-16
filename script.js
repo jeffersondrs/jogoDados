bb676b68b2dc7bfdf11a373f079d3fb48ad21f92
jeffersondrs <jeffersonduarte@id.uff.br>
2022-04-16 00:06:00 -0300

    up

--- script.js
+++ script.js
@@ -7,11 +7,18 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
 //Começando as condiçoes
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
 
 // funcionalidade dos dados rolando
 btnRoll.addEventListener('click', function(){
@@ -22,7 +29,16 @@ btnRoll.addEventListener('click', function(){
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
 
-    //3 checar o dado rolado 1 : if true, switch to next player 
+    //3 checar o dado rolado 1 
+    if(dice !== 1){
+        // adiciona o numero do dado ao score
+        currentScore += dice;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    }else{
+      //switch to next player
+      activePlayer = activePlayer === 0 ? 1 : 0;
+      currentScore = 0;
+    }
 })
 
 
